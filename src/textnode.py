4ec94eb2af943fd486becc9e68df480213b63285
rjames187 <rory.james2021@gmail.com>
2024-04-13 13:38:43 -0500

    write equality method of textnode class

--- src/textnode.py
+++ src/textnode.py
@@ -1,5 +1,14 @@
 class TextNode:
-  def __init__(self, text:str, text_type: str, url: str=None):
+  def __init__(self, text:str, text_type: str, url: str=None) -> None:
     self.text = text
     self.text_type = text_type
     self.url = url
+  
+  def __eq__(self, node1: "TextNode", node2: "TextNode") -> bool:
+    if node1.text != node2.text:
+      return False
+    if node1.text_type != node2.text_type:
+      return False
+    if node1.url != node1.url:
+      return False
+    return True
