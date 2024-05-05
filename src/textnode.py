49d880a59a9f2da8aa43e5f3a412a1b8cd4d6cc0
rjames187 <rory.james2021@gmail.com>
2024-05-04 22:43:49 -0500

    write textnode to htmlnode func

--- src/textnode.py
+++ src/textnode.py
@@ -1,3 +1,5 @@
+from htmlnode import LeafNode
+
 class TextNode:
   def __init__(self, text:str, text_type: str, url: str=None) -> None:
     self.text = text
@@ -15,3 +17,21 @@ class TextNode:
   
   def __repr__(self) -> str:
     return f'TextNode({self.text}, {self.text_type}, {self.url})'
+
+def text_node_to_html_node(text_node: TextNode) -> LeafNode:
+  text_type = text_node.text_type
+  text = text_node.text
+  if text_type == "text":
+    return LeafNode(None, text)
+  elif text_type == "bold":
+    return LeafNode("b", text)
+  elif text_type == "italic":
+    return LeafNode("i", text)
+  elif text_type == "code":
+    return LeafNode("code", text)
+  elif text_type == "link":
+    return LeafNode("a", text, {"href": text_node.url})
+  elif text_type == "image":
+    return LeafNode("img", None, {"src": text_node.url, "alt": text})
+  else:
+    raise Exception("Text node type must be bold, italic, code, link, or image")
