c44b3cef820bae92b480f4258c64666907fa1a80
rjames187 <rory.james2021@gmail.com>
2024-05-06 14:47:40 -0500

    create text to textnode func

--- src/textnode.py
+++ src/textnode.py
@@ -56,6 +56,9 @@ def extract_markdown_links(text: str) -> 'tuple[list[str]]':
 def split_nodes_image(old_nodes: 'list[TextNode]') -> 'list[TextNode]':
   res = []
   for old_node in old_nodes:
+    if old_node.text_type != text_type_text:
+      res.append(old_node)
+      continue
     text = old_node.text
     images = extract_markdown_images(text)
     if len(images) == 0:
@@ -74,6 +77,9 @@ def split_nodes_image(old_nodes: 'list[TextNode]') -> 'list[TextNode]':
 def split_nodes_link(old_nodes: 'list[TextNode]') -> 'list[TextNode]':
   res = []
   for old_node in old_nodes:
+    if old_node.text_type != text_type_text:
+      res.append(old_node)
+      continue
     text = old_node.text
     links = extract_markdown_links(text)
     if len(links) == 0:
@@ -89,20 +95,28 @@ def split_nodes_link(old_nodes: 'list[TextNode]') -> 'list[TextNode]':
       res.append(TextNode(text, text_type_text))
   return res
 
+def text_to_textnodes(text: str) -> 'list[TextNode]':
+  nodes = split_nodes_image([TextNode(text, text_type_text)])
+  nodes = split_nodes_link(nodes)
+  nodes = split_nodes_delimiter(nodes, '`', text_type_code)
+  nodes = split_nodes_delimiter(nodes, '**', text_type_bold)
+  nodes = split_nodes_delimiter(nodes, '*', text_type_italic)
+  return nodes
+
 def text_node_to_html_node(text_node: TextNode) -> LeafNode:
   text_type = text_node.text_type
   text = text_node.text
-  if text_type == "text":
+  if text_type == text_type_text:
     return LeafNode(None, text)
-  elif text_type == "bold":
+  elif text_type == text_type_bold:
     return LeafNode("b", text)
-  elif text_type == "italic":
+  elif text_type == text_type_italic:
     return LeafNode("i", text)
-  elif text_type == "code":
+  elif text_type == text_type_code:
     return LeafNode("code", text)
-  elif text_type == "link":
+  elif text_type == text_type_link:
     return LeafNode("a", text, {"href": text_node.url})
-  elif text_type == "image":
+  elif text_type == text_type_image:
     return LeafNode("img", None, {"src": text_node.url, "alt": text})
   else:
     raise Exception("Text node type must be bold, italic, code, link, or image")
