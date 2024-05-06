18006ce9237a1d0ff59c6d3b56eb1f313b369bfc
rjames187 <rory.james2021@gmail.com>
2024-05-06 17:22:07 -0500

    create func for copying static content ot public dir

--- src/main.py
+++ src/main.py
@@ -1,1 +1,16 @@
-print('hello world')
+from textnode import TextNode
+import shutil
+import os
+
+def copy():
+  cwd = os.getcwd()
+  static_path = os.path.join(cwd, 'static')
+  public_path = os.path.join(cwd, 'public')
+  if os.path.exists(public_path):
+    shutil.rmtree(public_path)
+  shutil.copytree(static_path, public_path)
+
+def main():
+  copy()
+
+main()
