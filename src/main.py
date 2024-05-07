77b7fe6cd8b7541aa2438d2907c5864c07f8140f
rjames187 <rory.james2021@gmail.com>
2024-05-06 20:19:44 -0500

    add generation of many pages

--- src/main.py
+++ src/main.py
@@ -12,7 +12,6 @@ def copy():
 
 def generate_page(from_path: str, template_path: str, dest_path: str):
   print(f"Generating page from {from_path} to {dest_path} using {template_path}")
-  from_path, template_path, dest_path = os.path.join(from_path), os.path.join(template_path), os.path.join(dest_path)
   try:
     md_f = open(from_path, mode='r')
     temp_f = open(template_path, mode='r')
@@ -20,21 +19,27 @@ def generate_page(from_path: str, template_path: str, dest_path: str):
     markdown = md_f.read()
     html = temp_f.read()
     content = markdown_to_html_node(markdown).to_html()
-    print(content)
     title = extract_title(markdown)
     html = html.replace('{{ Title }}', title)
     html = html.replace('{{ Content }}', content)
     dest_f.write(html)
   except Exception as e:
-    print(type(e))
     print(e)
   finally:
     md_f.close()
     temp_f.close()
     dest_f.close()
 
+def generate_many_pages(from_path: str, template_path: str, dest_path: str):
+  from_path, template_path, dest_path = os.path.join(from_path), os.path.join(template_path), os.path.join(dest_path)
+  for path in os.listdir(from_path):
+    if path.endswith('.md'):
+      new_from_path = os.path.join(from_path, path)
+      new_dest_path = os.path.join(dest_path, f'{path[:-2]}html' )
+      generate_page(new_from_path, template_path, new_dest_path)
+
 def main():
   copy()
-  generate_page('content/index.md', 'template.html', 'public/index.html')
+  generate_many_pages('content/', 'template.html', 'public/')
 
 main()
